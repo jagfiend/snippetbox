6ca74fd4d9a18d27bf32c5d180bce817f95405c1
Pete Wond <pete@alberon.co.uk>
2025-04-21 21:33:38 +0100

    logout

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -220,5 +220,16 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "handle logout")
+	err := app.sessionManager.RenewToken(r.Context())
+
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "You've been successfully logged out!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
